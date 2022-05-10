0870ccfd2b5a28cbfe7c488f0216755759f1aefc
xiaohongbo <xiaohongbo@yushu.biz>
2022-05-10 13:41:49 +0800

    feat:  着色器程序编写

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import "./style/index.scss";
+import { createShader } from "./gl";
 
-const warp: HTMLElement = <HTMLElement>document.getElementById("warp");
-const rect: DOMRect = <DOMRect>warp.getBoundingClientRect();
+const warp: HTMLElement = document.getElementById("warp");
+const rect: DOMRect = warp.getBoundingClientRect();
 
 // 创建一个canvas append到warp 容器中
-const canvas: HTMLCanvasElement = <HTMLCanvasElement>(
-  document.createElement("canvas")
-);
+const canvas: HTMLCanvasElement = document.createElement("canvas");
 warp.appendChild(canvas);
 canvas.height = rect.height;
 canvas.width = rect.width;
+
+const gl: WebGLRenderingContext = canvas.getContext("webgl");
+console.log(gl);
+console.log(createShader);
+
+// 使用createShaer 创建两个着色器
