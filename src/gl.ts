bc71ddf9267346909573acb1eddf3dae32914b8e
xiaohongbo <xiaohongbo@yushu.biz>
2022-05-11 13:37:57 +0800

    style:  使用tab缩进

--- src/gl.ts
+++ src/gl.ts
@@ -1,24 +1,24 @@
-import * as cubeShader from "./shaders/cube/vertex.glsl";
-import * as cubFragMent from "./shaders/cube/fragment.glsl";
-console.log("cubeShader", cubeShader);
-console.log("cubFragMent", cubFragMent);
+import * as cubeShader from './shaders/cube/vertex.glsl';
+import * as cubFragMent from './shaders/cube/fragment.glsl';
+console.log('cubeShader', cubeShader);
+console.log('cubFragMent', cubFragMent);
 
 // 创建着色器方法，输入参数：渲染上下文，着色器类型，数据源
 export function createShader(
-  gl: WebGLRenderingContext,
-  type: number,
-  source: string
+	gl: WebGLRenderingContext,
+	type: number,
+	source: string
 ): WebGLShader {
-  const shader: WebGLShader = gl.createShader(type); // 创建着色器对象
-  gl.shaderSource(shader, source); // 提供数据源
-  gl.compileShader(shader); // 编译 -> 生成着色器
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
-  }
+	const shader: WebGLShader = gl.createShader(type); // 创建着色器对象
+	gl.shaderSource(shader, source); // 提供数据源
+	gl.compileShader(shader); // 编译 -> 生成着色器
+	const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+	if (success) {
+		return shader;
+	}
 
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
+	console.log(gl.getShaderInfoLog(shader));
+	gl.deleteShader(shader);
 }
 
 /**
@@ -29,19 +29,19 @@ export function createShader(
  * @returns WebGLProgram
  */
 export function createProgram(
-  gl: WebGL2RenderingContext,
-  vertexShader: WebGLShader,
-  fragmentShader: WebGLShader
+	gl: WebGL2RenderingContext,
+	vertexShader: WebGLShader,
+	fragmentShader: WebGLShader
 ): WebGLProgram {
-  const program: WebGLProgram = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
+	const program: WebGLProgram = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+	if (success) {
+		return program;
+	}
 
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
+	console.log(gl.getProgramInfoLog(program));
+	gl.deleteProgram(program);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import "./style/index.scss";
-import { createShader } from "./gl";
+import './style/index.scss';
+import { createShader } from './gl';
 
-const warp: HTMLElement = document.getElementById("warp");
+const warp: HTMLElement = document.getElementById('warp');
 const rect: DOMRect = warp.getBoundingClientRect();
 
 // 创建一个canvas append到warp 容器中
-const canvas: HTMLCanvasElement = document.createElement("canvas");
+const canvas: HTMLCanvasElement = document.createElement('canvas');
 warp.appendChild(canvas);
 canvas.height = rect.height;
 canvas.width = rect.width;
 
-const gl: WebGLRenderingContext = canvas.getContext("webgl");
+const gl: WebGLRenderingContext = canvas.getContext('webgl');
 console.log(gl);
 console.log(createShader);
 
